09e83867aeb020d904aff9264947f8cb6ff449e1
Live session user <rodinhart@gmail.com>
2022-04-11 16:44:41 +0100

    Initial commit

---
+++ start.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt install git
+sudo timedatectl set-timezone Europe/London
+# setxkbmap uk
+curl -o chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes ./chrome.deb
+
