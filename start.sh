cc569f60b26b5147f4a6acbbb225cf364626b017
Rodin van der Hart <rodinhart@gmail.com>
2022-04-12 17:23:59 +0100

    Update start.sh
    Added instructions

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
+# sudo apt install git
+# git clone http://github.com/rodinhart/linux.git
+# cd linux
+# sh start.sh
 sudo apt-get update
 sudo apt install git
 sudo timedatectl set-timezone Europe/London
@@ -6,4 +10,4 @@ curl -o chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_curre
 sudo apt install --assume-yes ./chrome.deb
 sudo apt install --assume-yes clang
 sudo apt install --assume-yes lld-8
-
+echo "All done!"
