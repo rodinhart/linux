c6c6ddaf98616b6bbb49955b15da1a3306e47630
Rodin <rodinhart@gmail.com>
2022-04-18 13:24:26 +0100

    Mandelbrot

--- add.c
+++ add.c
@@ -6,3 +6,27 @@ float sum(float xs[]) {
   return xs[0] + xs[1] + xs[2];
 }
 
+void draw(unsigned char *img, int W, int H) {
+  double q = 4.0 / H;
+  for (int sy = 0; sy < H; sy += 1 ) {
+    double ry = -2.0 + q * sy;
+    for (int sx = 0; sx < W; sx += 1) {
+      double rx = -2.0 + q * sx;
+      int n = 256;
+      double a = rx, b = ry, c, d;
+      do {
+        c = a * a;
+        d = b * b;
+        b = 2 * a * b + ry;
+        a = c - d + rx;
+        n -= 1;
+      } while (c + d < 4 && n > 0);
+
+      int p = (sx + W * sy) * 4;
+      img[p + 0] = n;
+      img[p + 1] = n;
+      img[p + 2] = n;
+      img[p + 3] = 255;
+    }
+  }
+}
--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-clang --target=wasm32 -c -o $1.o $1
-wasm-ld-8 --no-entry --export add -export sum -o $1.wasm $1.o
+clang --target=wasm32 -c -O2 -o $1.o $1
+wasm-ld-8 --no-entry --export add --export sum --export draw -o $1.wasm $1.o
