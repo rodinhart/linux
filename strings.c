fcefbb235cea6041d489cbb68a5960cd3520e53f
Rodin van der Hart <rodin.hart@orgvue.com>
2022-08-08 09:06:03 +0100

    Refactored to separate javascript function

--- strings.c
+++ strings.c
@@ -4,8 +4,10 @@
 typedef unsigned char *string;
 typedef unsigned int uint;
 
-int stringCompare(string a, string b) {
-  while (*a != 0xFF && *b != 0xFF && *a == *b) {
+int stringCompare(string a, string b)
+{
+  while (*a != 0xFF && *b != 0xFF && *a == *b)
+  {
     a++;
     b++;
   }
@@ -13,59 +15,73 @@ int stringCompare(string a, string b) {
   return *a == *b;
 }
 
-uint hashCode(string key) {
+uint hashCode(string key)
+{
   uint hashval;
 
-  for (hashval = 0; *key != 0xFF; key++) {
+  for (hashval = 0; *key != 0xFF; key++)
+  {
     hashval = *key + 31 * hashval;
   }
 
   return hashval % SIZE;
 }
 
-string search(string hashArray[], string key) {
+string search(string hashArray[], string key)
+{
   uint hashIndex = hashCode(key);
   int count = 0;
 
-  while (hashArray[hashIndex] != NULL) {
-    if (stringCompare(hashArray[hashIndex], key)) {
+  while (hashArray[hashIndex] != NULL)
+  {
+    if (stringCompare(hashArray[hashIndex], key))
+    {
       return hashArray[hashIndex];
     }
 
     hashIndex = (hashIndex + 1) % SIZE;
 
     count++;
-    if (count == 16) break;
+    if (count == 16)
+      break;
   }
 
   return NULL; // return place to insert?
 }
 
-void insert(string hashArray[], string key) {
+void insert(string hashArray[], string key)
+{
   uint hashIndex = hashCode(key);
   int count = 0;
 
-  while (hashArray[hashIndex] != NULL) {
+  while (hashArray[hashIndex] != NULL)
+  {
     hashIndex = (hashIndex + 1) % SIZE;
-    
+
     count++;
-    if (count == 16) break;
+    if (count == 16)
+      break;
   }
 
   hashArray[hashIndex] = key;
 }
 
-
-int compact(int strings[], int len, string source, int strings2[], string target, string hashArray[]) {
+int compact(string target, int offsets[], int count, string source, string hashArray[])
+{
   string end = target;
-  for (int i = 0; i < len; i += 1) {
-    string match = search(hashArray, source + strings[i]);
-    if (match) {
-      strings2[i] = match - target;
-    } else {
-      string src = source + strings[i];
-      strings2[i] = end - target;
-      while (*src != 0xFF) {
+  for (int i = 0; i < count; i += 1)
+  {
+    string match = search(hashArray, source + offsets[i]);
+    if (match)
+    {
+      offsets[i] = match - target;
+    }
+    else
+    {
+      string src = source + offsets[i];
+      offsets[i] = end - target;
+      while (*src != 0xFF)
+      {
         *end = *src;
         src += 1;
         end += 1;
@@ -73,7 +89,7 @@ int compact(int strings[], int len, string source, int strings2[], string target
 
       *end = 0xFF;
       end += 1;
-      insert(hashArray, target + strings2[i]);
+      insert(hashArray, target + offsets[i]);
     }
   }
 
